f460494a956089f8b8d76535704d55513c746c05
fjknat <pnthaiit@gmail.com>
2017-09-07 23:53:02 +0700

    v1

--- Ex4 - File system/create_file.js
+++ Ex4 - File system/create_file.js
@@ -19,15 +19,36 @@ var requestListener = (req, rep) =>{
         };                    
     });
 
-    fs.open('file2.txt', 'w+', (err,  fileSave)=>{
+    fs.open('file2.txt', 'a+', (err,  fileSave)=>{
         if (err){
             rep.writeHead(200, {'Content-Type':'text/html'});
             rep.write(err.message);
             console.log(err.message);
             rep.end();
         } ;
-        fs.write(fileSave, )
+        fs.write(fileSave, 'test 12', 0, 'UTF8', (err, wr) =>{
+            let buf = new Buffer(1024);
+            fs.read(fileSave, buf, 0, buf.length, 0, (err, byt) => {
+                if(byt > 0){
+                    /* rep.writeHead(200, {'Content-Type':'text/html'});
+                    rep.write("<br>");
+                    rep.write(buf.slice(0, byt).toString());                    
+                    rep.end(); */
+                    console.log(buf.slice(0, byt).toString());
+                }
+            })
+        });
     });
+
+    fs.writeFile('test3.txt', 'tes data 3', (err) => {
+        if(err) throw err;
+        fs.renameSync('test3.txt', 'test3_a.txt', (err)=>{
+            if(err) throw err;        
+        });
+        fs.unlinkSync('test3_a.txt');
+    });
+
+    
     
     
 };
